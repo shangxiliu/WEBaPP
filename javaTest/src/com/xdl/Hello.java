2f755f1f963a01d0027f710bfd21dd849dd1a744
xingdiliang <weiqi.liu@aliyun.com>
2022-03-01 13:57:23 +0800

    Initial commit

---
+++ javaTest/src/com/xdl/Hello.java
@@ -0,0 +1,8 @@
+package com.xdl;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        System.out.println("liuweiqi");
+    }
+}
---
+++ web02/src/main/java/com/xdl/Wed02Servlet.java
@@ -0,0 +1,19 @@
+package com.xdl;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "Wed02Servlet", value = "/Wed02Servlet")
+public class Wed02Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
